92bbe203cb461c17a691c189e53375efe2d90633
VictorCerezini <victorcerezini@hotmail.com>
2026-04-19 16:31:13 -0300

    Created a new test example.
    Signed-off-by: VictorCerezini <victorcerezini@hotmail.com>

---
+++ examples/test.rs
@@ -0,0 +1,99 @@
+use bevy::{
+    prelude::{
+        App, Asset, AssetServer, Commands, DefaultPlugins, Entity, Handle, Image, Material,
+        PluginGroup, Res, ResMut, Startup, Transform, TransformPlugin, TypePath, Vec3, vec,
+    },
+    render::render_resource::{AsBindGroup, ShaderType, TextureDimension, TextureFormat},
+    shader::ShaderRef,
+};
+use bevy_terrain::prelude::{
+    BigSpaceCommands, CameraMode, DebugCameraController, Grid, LoadingImages,
+    OrbitalCameraController, SpawnTerrainCommandsExt, TerrainDebugPlugin, TerrainMaterialPlugin,
+    TerrainPickingPlugin, TerrainPlugin, TerrainSettings, TerrainViewConfig,
+};
+
+const VIEW_DISTANCE: f64 = 10000.;
+
+#[cfg(feature = "wesl")]
+const FRAGMENT_SHADER_ASSET_PATH: &str = "shaders/planar.wesl";
+
+#[cfg(not(feature = "wesl"))]
+const FRAGMENT_SHADER_ASSET_PATH: &str = "shaders/planar.wgsl";
+
+#[derive(ShaderType, Clone)]
+struct GradientInfo {
+    mode: u32,
+}
+
+#[derive(Asset, AsBindGroup, TypePath, Clone)]
+pub struct CustomMaterial {
+    #[texture(0)]
+    #[sampler(1)]
+    gradient: Handle<Image>,
+    #[uniform(2)]
+    gradient_info: GradientInfo,
+}
+
+impl Material for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        FRAGMENT_SHADER_ASSET_PATH.into()
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins.build().disable::<TransformPlugin>(),
+            TerrainPlugin,
+            TerrainMaterialPlugin::<CustomMaterial>::default(),
+            TerrainDebugPlugin,
+            TerrainPickingPlugin,
+        ))
+        .insert_resource(TerrainSettings::new(vec!["Albedo"]))
+        .add_systems(Startup, initialize)
+        .run();
+}
+
+#[allow(clippy::too_many_arguments)]
+fn initialize(
+    mut commands: Commands,
+    mut images: ResMut<LoadingImages>,
+    asset_server: Res<AssetServer>,
+) {
+    let gradient1 = asset_server.load("textures/gradient1.png");
+    images.load_image(
+        &gradient1,
+        TextureDimension::D2,
+        TextureFormat::Rgba8UnormSrgb,
+    );
+
+    let gradient2 = asset_server.load("textures/gradient2.png");
+    images.load_image(
+        &gradient2,
+        TextureDimension::D2,
+        TextureFormat::Rgba8UnormSrgb,
+    );
+
+    let mut view = Entity::PLACEHOLDER;
+
+    commands.spawn_big_space(Grid::default(), |root| {
+        view = root
+            .spawn_spatial((
+                Transform::from_translation(Vec3::new(0.0, VIEW_DISTANCE as f32, 0.0))
+                    .looking_to(Vec3::NEG_Y, Vec3::NEG_Z),
+                DebugCameraController::new(VIEW_DISTANCE),
+                OrbitalCameraController::new(CameraMode::Planar),
+            ))
+            .id();
+    });
+
+    commands.spawn_terrain(
+        asset_server.load("terrains/test/config.tc.ron"),
+        TerrainViewConfig::default(),
+        CustomMaterial {
+            gradient: gradient1.clone(),
+            gradient_info: GradientInfo { mode: 2 },
+        },
+        view,
+    );
+}
---
+++ preprocess/examples/preprocess_test.rs
@@ -0,0 +1,69 @@
+use std::env::set_var;
+
+use bevy_terrain::prelude::{AttachmentFormat, AttachmentLabel};
+use bevy_terrain_preprocess::{PreprocessData, prelude::{
+    Cli, PreprocessContext, PreprocessDataType, PreprocessNoData, preprocess
+}};
+use gdal::raster::GdalDataType;
+
+fn main() {
+    unsafe {
+        if true {
+            set_var("RAYON_NUM_THREADS", "0");
+            set_var("GDAL_NUM_THREADS", "ALL_CPUS");
+        } else {
+            set_var("RAYON_NUM_THREADS", "1");
+            set_var("GDAL_NUM_THREADS", "1");
+        }
+    }
+
+    let args1 = Cli {
+        src_path: vec!["assets/source_data/Volcano_Out.tif".into()],
+        terrain_path: "../assets/terrains/test".into(),
+        temp_path: None,
+        overwrite: true,
+        no_data: PreprocessNoData::Source,
+        data_type: PreprocessDataType::DataType(GdalDataType::Float32),
+        fill_radius: 16.0,
+        create_mask: true,
+        lod_count: None,
+        attachment_label: AttachmentLabel::Height,
+        texture_size: 512,
+        border_size: 2,
+        side_length: 10000.,
+        height_scale: 2500.,
+        radius: None,
+        mip_level_count: 1,
+        format: AttachmentFormat::R32F,
+    };
+
+    let args2 = Cli {
+        src_path: vec!["assets/source_data/SuperColor_Out.tif".into()],
+        terrain_path: "../assets/terrains/test".into(),
+        temp_path: None,
+        overwrite: true,
+        no_data: PreprocessNoData::Source,
+        data_type: PreprocessDataType::DataType(GdalDataType::UInt8),
+        fill_radius: 16.0,
+        create_mask: false,
+        lod_count: None,
+        attachment_label: AttachmentLabel::Custom("Albedo".to_string()),
+        texture_size: 512,
+        border_size: 2,
+        side_length: 10000.,
+        height_scale: 2500.,
+        radius: None,
+        mip_level_count: 1,
+        format: AttachmentFormat::Rgba8U,
+    };
+
+    let mut data_list: Vec<PreprocessData> = [args1, args2]
+    .into_iter()
+    .map(|args| {
+        let (dataset, context) = PreprocessContext::from_cli(args).unwrap();
+        PreprocessData { dataset, context }
+    })
+    .collect();
+
+    preprocess(&mut data_list);
+}
